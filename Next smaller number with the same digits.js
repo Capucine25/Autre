b74a61e803051690849542c104c1dc0f50a95758
Capucine25 <40346652+Capucine25@users.noreply.github.com>
2018-06-23 18:24:19 -0400

    Update Next smaller number with the same digits.js

--- Next smaller number with the same digits.js
+++ Next smaller number with the same digits.js
@@ -18,32 +18,24 @@ function nextSmaller(n){
     }
   }
   
-  console.log(list)
-  if (Number(numbers.join(""))==n){
+  list.sort((a,b)=>a-b)
+
+  
+  if (list.length==0){
     return -1
   }
   
+  if ((list[(list.length-1)].toString().split("").length)
+  !=(n.toString().split("").length)){
+      return -1}
   
+
   if (list.length==1){
     return list[0]
     
   }
   
   else {
-    list.sort((a,b)=>a-b)
-    if ((list[(list.length-1)].toString().split("").length)!=(n.toString().split("").length)){
-      return -1
-    }
-    else{
     return list[list.length-1]
       
     }
-  }
-  
-}
-
-console.log(nextSmaller(1027))
-//629973385
-
-console.log(nextSmaller(985823629973538))
-//985823629973385
